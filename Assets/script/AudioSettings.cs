097eacfd6982277e300ffc0648b47edca0657c15
trolleyrevolution <helloemirsofyan2@gmail.com>
2026-09-12 17:11:19 +0700

    Update

---
+++ Assets/script/AudioSettings.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource BGM;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
