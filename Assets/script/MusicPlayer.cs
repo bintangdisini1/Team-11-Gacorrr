ab356f899e6be5bda6e9721bb404b9851e49245e
trolleyrevolution <helloemirsofyan2@gmail.com>
2026-09-12 14:23:29 +0700

    Main Menu

---
+++ Assets/script/MusicPlayer.cs
@@ -0,0 +1,21 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float BGMVolume, SFXVolume, voiceVolume;
+    public int volumeSlider;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
