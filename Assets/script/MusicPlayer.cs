22ebc50ab99ddcee6fb5e8e3d11982249616874e
Bintang <osasbintang@gmail.com>
2026-09-12 23:05:16 +0700

    Revert "update"
    This reverts commit 94ec5ee11fc0069142a9e19ba94e68b0a2dccf0b.

--- Assets/script/MusicPlayer.cs
+++ Assets/script/MusicPlayer.cs
@@ -1,21 +1,39 @@
-using JetBrains.Annotations;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicPlayer : MonoBehaviour
 {
+    [SerializeField] Slider volumeSlider;
+
     // Start is called before the first frame update
-    public float BGMVolume, SFXVolume, voiceVolume;
-    public int volumeSlider;
     void Start()
     {
-
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeVolume()
     {
-        
+        Transform parent = transform.parent;
+        Transform bgm = parent.Find("BGM");
+        AudioSource themusic = bgm.GetComponent<AudioSource>();
+        themusic.volume = volumeSlider.value;
+        Save();
+    }
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 }
